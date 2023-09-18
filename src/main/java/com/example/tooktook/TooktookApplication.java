d144ddf28a9e7ce42263de83c1daef6646c4a7c5
TaehoLEEKR <679748@naver.com>
2023-09-18 17:20:43 +0900

    first commit

---
+++ src/main/java/com/example/tooktook/TooktookApplication.java
@@ -0,0 +1,13 @@
+package com.example.tooktook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooktookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TooktookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tooktook/TooktookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tooktook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooktookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

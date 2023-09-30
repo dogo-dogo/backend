6cdfb13cd4698873febcd0fabb8e8757b34fa9dc
TaehoLEEKR <679748@naver.com>
2023-09-30 14:29:29 +0900

    feat : neo4j 기능 구현 (#13)
    body: neo4j 기능 구현
    
    user 가 등록 하였을 때
    기본 닉네임 설정 및 유효성 검사
    user 의 그래프 디비 등록
    user 의 기본 질문 생성
    user 의 답변 생성시 그래프 질문에 답변 추가

--- src/main/java/com/example/tooktook/controller/AuthController.java
+++ src/main/java/com/example/tooktook/controller/AuthController.java
@@ -9,7 +9,6 @@ import com.example.tooktook.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 
---
+++ src/main/java/com/example/tooktook/model/dto/QuestionDTO.java
@@ -0,0 +1,12 @@
+package com.example.tooktook.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionDTO {
+    private Long questionId;
+    private String questionText;
+
+}

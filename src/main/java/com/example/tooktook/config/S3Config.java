5a93263f3f804c4bf68a9c505672869ce37d2325
서세란 <dear_u3@naver.com>
2023-10-22 15:34:57 +0900

    refactor : S3&Springboot 연동3
    body :

--- src/main/java/com/example/tooktook/config/S3Config.java
+++ src/main/java/com/example/tooktook/config/S3Config.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.validation.Valid;
-
 @Configuration
 public class S3Config {
 
@@ -35,4 +33,5 @@ public class S3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
+
 }

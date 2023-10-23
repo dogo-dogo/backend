95ef9a11eaba68fff90ece40de5262b277d1a3c4
LeeDaeHo <679748@naver.com>
2023-10-23 18:23:43 +0900

    refactor : JwtToken 사용방식 변경 , Bug Fix
    Body : JwtToken 사용법 변경 , optionalText 가 안붙는 버그 Fix
    
    issue : {# 32}

--- src/main/java/com/example/tooktook/controller/MemberController.java
+++ src/main/java/com/example/tooktook/controller/MemberController.java
@@ -3,7 +3,10 @@ package com.example.tooktook.controller;
 import com.example.tooktook.common.response.ApiResponse;
 import com.example.tooktook.common.response.ResponseCode;
 import com.example.tooktook.model.repository.MemberNeo4jRepository;
+import com.example.tooktook.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +19,20 @@ import javax.validation.constraints.Pattern;
 @RequestMapping("/api/member")
 public class MemberController {
 
-//    private final MemberNeo4jRepository memberNeo4jRepository;
-//    private final MemberService memberService;
-//    @PutMapping("/nickname/{memberId}")
-//    public void setNickName(
-//            @RequestParam @Valid
-//            @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,6}$", message = "닉네임은 2~6자의 영문, 숫자, 한글만 사용 가능합니다.")
-//            String nickName,
-//            @PathVariable("memberId") Long memberId
-//    ) {
-//        memberService.setNickName(nickName, memberId);
-//    }
+    private final MemberNeo4jRepository memberNeo4jRepository;
+    private final MemberService memberService;
+    @PutMapping("/nickname/{memberId}")
+    public ApiResponse<?> setNickName(
+            @RequestParam @Valid @Pattern(regexp = "^[a-zA-Z0-9가-힣]{2,6}$", message = "닉네임은 2~6자의 영문, 숫자, 한글만 사용 가능합니다.")
+            String nickName,
+            @AuthenticationPrincipal UserDetails loginMember
+    ) {
+
+        Long memberId = memberNeo4jRepository.findByLoginEmail(loginMember.getUsername())
+                .get().getMemberId();
+
+        memberService.setNickName(nickName, memberId);
+        return ApiResponse.ok(ResponseCode.Normal.UPDATE,memberId);
+    }
 
 }
--- src/main/java/com/example/tooktook/service/MemberService.java
+++ src/main/java/com/example/tooktook/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.tooktook.service;
 
+import com.example.tooktook.common.response.ResponseCode;
 import com.example.tooktook.exception.ErrorCode;
 import com.example.tooktook.exception.GlobalException;
 import com.example.tooktook.model.entity.Member;
@@ -15,7 +16,7 @@ public class MemberService {
 
     public void setNickName(String nickName, Long memberId){
         Member member = memberNeo4jRepository.findByMemberId(memberId)
-                .orElseThrow( () -> new GlobalException(ErrorCode.NOT_FIND_MEMBER_ID));
+                .orElseThrow( () -> new GlobalException(ResponseCode.ErrorCode.NOT_FIND_MEMBER));
         member.setNickname(nickName);
         memberNeo4jRepository.save(member);
     }

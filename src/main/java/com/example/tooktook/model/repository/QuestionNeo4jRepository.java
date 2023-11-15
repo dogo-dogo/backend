6d9990b4e3b25e86f16caa545f6bcb04cc77015f
LeeDaeHo <679748@naver.com>
2023-11-15 23:06:03 +0900

    fix : list 화
    body : .

--- src/main/java/com/example/tooktook/model/repository/QuestionNeo4jRepository.java
+++ src/main/java/com/example/tooktook/model/repository/QuestionNeo4jRepository.java
@@ -23,7 +23,7 @@ public interface QuestionNeo4jRepository extends Neo4jRepository<Question, Long>
 //    List<QuestionDto> findCategoryIdToQuestion(@Param("memberId")Long memberId,@Param("cid") Long cid);
     @Query("MATCH (m:Member)-[:CATEGORY]->(c:Category)-[:ASKS]->(q:Question)-[:HAS_ANSWER]->(a:Answer) " +
             "WHERE id(m) = $memberId AND id(c) = $cid " +
-            "WITH id(q) AS qid, q.text AS questions, COLLECT(id(a)) AS aidList " +
+            "WITH id(q) AS qid, q.text AS questions, COLLECT(id(a)) AS answerIds " +
             "RETURN qid, questions, aidList;")
     List<QuestionDto> findCategoryIdToQuestion(@Param("memberId")Long memberId,@Param("cid") Long cid);
 

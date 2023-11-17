b4e6427eb02ef6f2077ed836a1b3e773067f2a90
LeeDaeHo <679748@naver.com>
2023-11-18 00:20:52 +0900

    .

--- src/main/java/com/example/tooktook/model/dto/answerDto/AnswerDto.java
+++ src/main/java/com/example/tooktook/model/dto/answerDto/AnswerDto.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.NotBlank;
 public class AnswerDto {
 
     @NotBlank(message = "질문에 답변을 입력 해주세요 ")
-    @Length(min = 2, max = 20, message = "필수 답변은 최소 2글자 ~ 20글자 입니다.")
+    @Length(max = 20, message = "필수 답변은 최소 2글자 ~ 20글자 입니다.")
     private String mainText;
     private String optionalText;
 }

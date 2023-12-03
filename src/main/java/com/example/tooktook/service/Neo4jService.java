089ab8b5e93f84647774221a81b24a9aad7c775a
LeeDaeHo <679748@naver.com>
2023-12-03 23:51:52 +0900

    2023/12/03-Prometeous, Grapana

--- src/main/java/com/example/tooktook/service/Neo4jService.java
+++ src/main/java/com/example/tooktook/service/Neo4jService.java
@@ -48,7 +48,6 @@ public class Neo4jService {
             for (CategoryEnum categoryEnum : CategoryEnum.values()) {
                 Category category = new Category(categoryEnum.getText());
                 member.addCategory(category);
-
                 switch (categoryEnum.getText()){
                     case "Bye 2023":
                         Arrays.stream(Bye2023.values())
